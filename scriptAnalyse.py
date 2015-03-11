33f1230af735c4f2f9c2bf3914afb0cdde25f399
Thibaut <toosolid2003@yahoo.fr>
2015-03-11 11:38:00 +0100

    Nettoyage du fichier source
    Exclusion des users qui ne sont pas SA du data set

--- scriptAnalyse.py
+++ scriptAnalyse.py
@@ -2,7 +2,66 @@ import numpy as np
 import pandas as pd
 
 #Création du dataframe
-df = pd.read_excel("Raw Data2.xlsx")
+df = pd.read_excel("/Users/thibaut.segura/Desktop/Raw Data.xlsx")
+
+
+#Formattage
+#####################################################################
+
+#Conversion des entrées de LOGIN_ID en intégrale, quand c'est possible
+def formatting(entree):
+    try:
+        entree = int(entree)
+    except:
+        pass
+    
+    return entree
+
+df["LOGIN_ID"] = df.LOGIN_ID.apply(lambda x: formatting(x))
+
+#Filtrage des données de LOGIN_ID: on ne retient que les Sales Associates, parce qu'ils sont au format 'int'
+SaRecordings = df[df["LOGIN_ID"].apply(lambda x: type(x) == int)]
+
+
+
+#Calculs    
+####################################################################
+#Nb de transactions réussies
+transacOk = SaRecordings["EMPLOYEE_ID"].value_counts().sum()
+transacTotal = SaRecordings["LOGIN_ID"].value_counts().sum()
+tauxSucces = transacOk / transacTotal
+
+#Type de transactions et nombres (résultat sous forme de Séries, équiv d'un dico)
+r = SaRecordings["API_ACTION"].value_counts()
+
+#Nombre de users différents qui sont parvenus à se connecter sur la période
+nbUsers = len(SaRecordings["EMPLOYEE_ID"].unique())
+
+#Ratios
+###Totaux des recherches
+totalSearch = r.sum() - r['Login'] - r['Logout'] - r['Changepwd'] - r['customerDetails']
+ItemSearchTotal = r['getInvPar'] + r['getInvStyle'] + r['getInvSku_manual'] + r['getInvSku_picture']
+
+ratioCustomerSearch = r['customerSearch'] / totalSearch
+ratioItemSearch = ItemSearchTotal / totalSearch
+
+ratioManualSku = r['getInvSku_manual'] / ItemSearchTotal
+ratioSkuPicture = r['getInvSku_picture'] / ItemSearchTotal
+
+
+#Output
+#####################################################################
+print('Nombre d\' utilisateurs uniques connectés: ', nbUsers)
+print('Taux de succès dans les transactions: ', tauxSucces)
+print("Customer Search: ", ratioCustomerSearch, "\n", "Item Search: ", ratioItemSearch)
+print('Manual Sku: ', ratioManualSku)
+print('SKU with pic: ', ratioSkuPicture)
+
+
+
+
+#Random stuff
+#####################################################################
 
 #Nombre de visiteurs uniques, Sales Associates uniquement
 #####################################################################
@@ -26,25 +85,3 @@ df = pd.read_excel("Raw Data2.xlsx")
 #    except ValueError:
 #        "Mauvaise valeur"
 #    indX += 1
-
-
-
-#Calculs    
-####################################################################
-#Nb de transactions réussies
-transacOk = df["EMPLOYEE_ID"].value_counts().sum()
-
-#Type de transactions et nombres (résultat sous forme de Séries, équiv d'un dico)
-r = df["API_ACTION"].value_counts()
-
-#Ratios
-###Totaux des recherches
-totalSearch = r['customerSearch'] + r['getInvPar'] + r['getInvStyle'] + r['getInvStyle'] + r['getInvSku_manual'] + r['getInvSku_picture']
-
-ratioCustomerSearch = r['customerSearch'] / totalSearch
-ratioItemSearch = (r['getInvPar'] + r['getInvStyle'] + r['getInvStyle'] + r['getInvSku_manual'] + r['getInvSku_picture']) / totalSearch
-
-
-#Output
-#####################################################################
-print("Customer Search: ", ratioCustomerSearch, "\n", "Item Search: ", ratioItemSearch)
